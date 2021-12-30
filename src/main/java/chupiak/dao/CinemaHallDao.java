b054d4d752c19103b8fcddf484f81341729c239f
Chupiak <chupyak@gmail.com>
2021-12-30 14:04:27 +0200

    first commit

---
+++ src/main/java/chupiak/dao/CinemaHallDao.java
@@ -0,0 +1,13 @@
+package chupiak.dao;
+
+import chupiak.model.CinemaHall;
+import java.util.List;
+import java.util.Optional;
+
+public interface CinemaHallDao {
+    CinemaHall add(CinemaHall cinemaHall);
+
+    Optional<CinemaHall> get(Long id);
+
+    List<CinemaHall> getAll();
+}
